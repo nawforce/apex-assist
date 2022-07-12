e324bed6141f815be1e1542fbb1f279061e6d489
Kevin Jones <nawforce@gmail.com>
2022-07-12 06:41:38 +0100

    Bundle extension

---
+++ webpack.dev.js
@@ -0,0 +1,35 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node',
+  entry: './dist/boot-dev.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode' 
+  },
+  resolve: {
+    mainFields: ['main'], 
+    extensions: ['.js'],
+  },
+  ignoreWarnings: [
+    {
+      module: /dist\/boot-dev.js/,
+    },
+    {
+      module: /@salesforce\/core\/lib\/messages.js/,
+    },
+  ]
+};
+module.exports = config;
